f84773240e30fa0590754600bfe76856b1d7e1e1
Isaac Granillo <isaac.granillo@gmail.com>
2015-11-30 10:58:05 -0800

    Initial Commit

---
+++ What Is The Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What Is The Weather
+//
+//  Created by Isaac Granillo on 11/30/15.
+//  Copyright © 2015 Isaac Granillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
